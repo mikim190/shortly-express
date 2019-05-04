d6b7af0a4f97c5fde4638b603073029b873d1524
mikim190 <mykim.tran297@gmai.com>
2019-05-03 20:13:09 -0700

    Implement cookie parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,30 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+
+  if (req.cookies.shortlyid) {
+    // if shortlyid is defined on req.cookies
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+      .then(result => {
+        if (result) {
+          req.session.hash = req.cookies.shortlyid;
+        }
+      })
+  }
+
+
+  //check if shortlyid is defined on req.cookies
+  //if defined,
+  // look up user data related to that session
+  // if valid, assign object to req.session
+  // if user data is not available .... ?  
+
+  //if no cookies, initialize new Session 
+  //by calling models.Sessions.create(), have it stored in sessions db
+  //set new cookies object on response headers 
+
+
+
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+
+  if (req.headers.cookie) {
+    var cookies = req.headers.cookie.split('; ');
+    for (var cookie of cookies) {
+      var data = cookie.split('=');
+      req.cookies[data[0]] = data[1];
+    }
+    next();
+  } else {
+    next();
+  }
 };
 
 module.exports = parseCookies;
+
+
